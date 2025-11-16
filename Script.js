4179116cdc8ecdeee54daba6fec959da18ab00ad
pankaj <mebssfzd99@gmail.com>
2025-11-16 14:03:30 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,39 @@
+/* css/style.css */
+
+body {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #ffffff;
+    color: #2c3e50;
+    line-height: 1.6;
+}
+
+header {
+    background-color: #3498db; /* Blue Header */
+    color: white;
+    padding: 40px 0;
+    text-align: center;
+}
+
+h1 {
+    font-size: 2.5em;
+    margin-bottom: 5px;
+}
+
+section {
+    padding: 40px 20px;
+    text-align: center;
+}
+
+#about {
+    background-color: #ecf0f1;
+}
+
+footer {
+    background-color: #2c3e50;
+    color: white;
+    padding: 10px 0;
+    text-align: center;
+    font-size: 0.9em;
+}

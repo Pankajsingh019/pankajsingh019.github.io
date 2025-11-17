e2d67588a551c7d881c413575d2fc68580c11bdc
pankaj <mebssfzd99@gmail.com>
2025-11-17 21:00:02 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+// POPUP MENU
 const openBtn = document.getElementById("openMenu");
 const closeBtn = document.getElementById("closeMenu");
 const popup = document.getElementById("popupMenu");
@@ -5,7 +6,26 @@ const menuItems = document.querySelectorAll(".pop-item");
 
 openBtn.onclick = () => popup.style.display = "block";
 closeBtn.onclick = () => popup.style.display = "none";
+menuItems.forEach(item => item.onclick = () => popup.style.display = "none");
 
-menuItems.forEach(item => {
-    item.onclick = () => popup.style.display = "none";
-});
+
+// THEME (Dark / Light Mode)
+const btn = document.getElementById("themeBtn");
+
+btn.onclick = () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        btn.textContent = "☀️";
+        localStorage.setItem("theme", "dark");
+    } else {
+        btn.textContent = "🌙";
+        localStorage.setItem("theme", "light");
+    }
+};
+
+// Load theme on refresh
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    btn.textContent = "☀️";
+}

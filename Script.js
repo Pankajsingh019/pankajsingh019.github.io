3e88a6b6b2cfa85784205826c802303362988810
pankaj <mebssfzd99@gmail.com>
2025-11-17 20:49:54 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,39 +1,11 @@
-/* css/style.css */
+const openBtn = document.getElementById("openMenu");
+const closeBtn = document.getElementById("closeMenu");
+const popup = document.getElementById("popupMenu");
+const menuItems = document.querySelectorAll(".pop-item");
 
-body {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    margin: 0;
-    padding: 0;
-    background-color: #ffffff;
-    color: #2c3e50;
-    line-height: 1.6;
-}
+openBtn.onclick = () => popup.style.display = "block";
+closeBtn.onclick = () => popup.style.display = "none";
 
-header {
-    background-color: #3498db; /* Blue Header */
-    color: white;
-    padding: 40px 0;
-    text-align: center;
-}
-
-h1 {
-    font-size: 2.5em;
-    margin-bottom: 5px;
-}
-
-section {
-    padding: 40px 20px;
-    text-align: center;
-}
-
-#about {
-    background-color: #ecf0f1;
-}
-
-footer {
-    background-color: #2c3e50;
-    color: white;
-    padding: 10px 0;
-    text-align: center;
-    font-size: 0.9em;
-}
+menuItems.forEach(item => {
+    item.onclick = () => popup.style.display = "none";
+});
